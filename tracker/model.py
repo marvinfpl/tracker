05ec918d7ccb16d5279a580ee947af0697325b67
marvinfpl <marvinfoppolo@gmail.com>
2026-08-19 15:33:19 +0200

    tracker class

--- tracker/model.py
+++ tracker/model.py
@@ -1,16 +1,27 @@
 import torch.nn as nn
 import os
 
-model = nn.Sequential(
-    nn.Conv2d(3, 16, 3, (2, 1)),
-    nn.MaxPool2d(5, 1),
-    nn.Conv2d(16, 32, 5, (2, 1)),
-    nn.MaxPool2d(3, 2),
-    nn.Conv2d(32, 128, 3, (2, 1)),
-    nn.Flatten(1),
-    nn.Linear(128, 256),
-    nn.ReLU(),
-    nn.Linear(256, 256),
-    nn.ReLU(),
-    nn.Linear(256, 2)
-)
+class Tracker(nn.Module):
+    def __init__(self):
+        super.__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 16, 3, (2, 1)),
+            nn.MaxPool2d(5, 1),
+            nn.Conv2d(16, 32, 5, (2, 1)),
+            nn.MaxPool2d(3, 2),
+            nn.Conv2d(32, 128, 3, (2, 1))
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(1),
+            nn.LazyLinear(256),
+            nn.ReLU(),
+            nn.Linear(256, 256),
+            nn.ReLU(),
+            nn.Linear(256, 2),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        return self.classifier(x)
