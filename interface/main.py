0534bdb8830db04f7c141b1929670bffe32c9f70
marvinfpl <marvinfoppolo@gmail.com>
2026-09-01 14:58:02 +0200

    camera controls

--- interface/main.py
+++ interface/main.py
@@ -1,44 +1,61 @@
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from fastapi import Request
 import cv2
 from tracker.yolo import detect_faces
 
 app = FastAPI()
 templates = Jinja2Templates(directory='interface/templates')
+app.mount(
+    '/static',
+    StaticFiles(directory='interface/templates'),
+    name='static',
+)
+
+stream = False
 
 def generate_frame():
+        global stream
         cap = cv2.VideoCapture(0)
+        try:
+            while stream:
+                ok, frame = cap.read()
 
-        while True:
-            ok, frame = cap.read()
+                if not ok:
+                   break
 
-            if not ok:
-                return
+                frame = detect_faces(frame)
+                ok, buffer = cv2.imencode('.jpg', frame)
 
-            frame = detect_faces(frame)
+                if not ok:
+                    continue
 
-            ok, buffer = cv2.imencode('.jpg', frame)
+                yield (
+                b"--frame\r\n"b"Content-Type: image/jpeg\r\n\r\n"
+                + buffer.tobytes()
+                + b"\r\n"
+                )
 
-            if not ok:
-                continue
-
-            frame_bytes = buffer.tobytes()
-
-            yield (
-               b"--frame\r\n"b"Content-Type: image/jpeg\r\n\r\n"
-               + frame_bytes
-               + b"\r\n"
-            )
+        finally:
+             cap.release()
 
 
-@app.get('/camera')
-def show_camera():
+@app.get('/start_camera')
+def start_camera():
+    global stream
+    stream = True
     return StreamingResponse(
         generate_frame(),
         media_type="multipart/x-mixed-replace; boundary=frame"
     )
+
+@app.get('/stop_camera')
+def stop_camera():
+    global stream
+    stream = False
+    return {'status': 'stopped'}
 
 @app.get('/')
 def home(request: Request):
---
+++ interface/templates/index.js
@@ -0,0 +1,19 @@
+const btn = document.getElementById('btn');
+const camera = document.getElementById('camera');
+let running = false;
+
+
+btn.addEventListener('click', async () => {
+    if (!running) {
+        camera.src = '/start_camera';
+        btn.textContent = 'Stop Camera';
+        running = true;
+        camera.style.display = 'block';
+    } else {
+        await fetch('/stop_camera');
+        camera.src = '';
+        btn.textContent = 'Start Camera';
+        running = false;
+        camera.style.display = 'none';
+    }
+});
